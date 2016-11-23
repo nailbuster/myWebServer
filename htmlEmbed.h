84a321fa3e4d7fdf6ce762fba7f5effc64dfbf0f
David Paiva <david@nailbuster.com>
2016-11-23 17:45:23 -0500

    support for linux with include typo....

--- htmlEmbed.h
+++ htmlEmbed.h
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define _HTMLEMBED_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+	#include "Arduino.h"
 #else
 	#include "WProgram.h"
 #endif
